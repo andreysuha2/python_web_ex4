576655ed62200b0c5cc6408f1a8b291df7683c5e
Andrii Sukhenko <andreysuha2@gmail.com>
2023-12-16 11:04:54 +0200

    save commit

--- ex_4/controllers/MainController.py
+++ ex_4/controllers/MainController.py
@@ -1,7 +1,10 @@
 from ex_4.core.server import HTTPRequestHandler
+import json
+import socket
 import urllib.parse
+import os 
 
-class MainController:
+class MainController:    
     def index(self, request: HTTPRequestHandler):
         request.send_html_file("index.html")
 
@@ -15,8 +18,19 @@ class MainController:
         print(data_parse)
         data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
         print(data_dict)
+        self.send_message(data_dict)
         request.send_response(302)
         request.send_header('Location', '/')
         request.end_headers()
+
+    def send_message(self, message):
+        data = json.dumps(message).encode('utf-8')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server_address = (os.getenv("UDP_HOST"), int(os.getenv("UDP_PORT")))
+        sock.sendto(data, server_address)
+        response, address = sock.recvfrom(1024)
+        print(f'Response data: {response.decode()} from address: {address}')
+        sock.close()
+
 
 main_controller = MainController()
--- ex_4/core/soketServer.py
+++ ex_4/core/soketServer.py
@@ -1,19 +1,26 @@
 import socket
+import json
+from ex_4.storage.storage import Storage
 
 class SocketServer():
     def __init__(self, HOST: str, PORT: int):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((HOST, PORT))
+        self.storage = Storage()
         print(f"UDP server started on {HOST}:{PORT}")
         
     def start(self):
-        while True:
-            try: 
-                data, address = self.sock.recv(1024)
+        try:
+            while True:
+                data, address = self.sock.recvfrom(1024)
+                print("Recive data:", data)
+                print("from:", address)
                 self.sock.sendto(data, address)
-            except socket.timeout:
-                continue
-            except KeyboardInterrupt:
-                print(f'UDP server destroyed')
-            finally:
-                self.sock.close()
+                self.storage.write_to_file(json.loads(data))
+        except KeyboardInterrupt:
+            self.sock.close() 
+                
+            
+    def close(self):
+        print(f'UDP server destroyed')
+        self.sock.close()
